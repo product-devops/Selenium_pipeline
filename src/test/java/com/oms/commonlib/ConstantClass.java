3b3b346cd49c04d0fb9d47a2d1c40f5eb7ba098b
product-devops <subrata.fouzder@accenture.com>
2017-05-17 14:57:47 +0530

    Update ConstantClass.java

--- src/test/java/com/oms/commonlib/ConstantClass.java
+++ src/test/java/com/oms/commonlib/ConstantClass.java
@@ -11,28 +11,12 @@ public class ConstantClass {
 	// Application URL
 	
 	public static String joshuaExtnPath = ".\\extensions\\chrome-ext.crx";
-	
-	chromedriver = "//usr//bin//chromedriver";
-		driver = webdriver.Chrome(chromedriver);
-		driver.get("http://www.google.com");
-		driver.quit();
-	//public static String chromeDriverPath = ".//usr//bin//chromedriver";
+	public static String chromeDriverPath = ".//usr//bin//chromedriver";
 
 	public static WebDriver globaldriver;
 
 	
 	//
 	public static final File projectDir =  new File(System.getProperty("user.dir"));
-
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-	
 
 }
