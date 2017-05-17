2de25a222f92a299b5fc089f4c563bafd860244a
product-devops <subrata.fouzder@accenture.com>
2017-05-17 15:54:51 +0530

    Update Driver.java

--- src/test/java/com/oms/genericlib/Driver.java
+++ src/test/java/com/oms/genericlib/Driver.java
@@ -3,6 +3,8 @@ package com.oms.genericlib;
 import java.io.File;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver; 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
